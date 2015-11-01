fc64a056d6722b42f90d58d3eaf6dff49045a51a
Ralph Pineda <ralph.pineda@gmail.com>
2015-11-01 17:39:39 +0800

    Changed tabs to spaces. Added hasPartyGuest func

--- PartyGuests.js
+++ PartyGuests.js
@@ -13,67 +13,71 @@
  */
 
 (function() {
-	// ========= Plugin interpreter commands setup =========
+    // ========= Plugin interpreter commands setup =========
     var _Game_Interpreter_pluginCommand = Game_Interpreter.prototype.pluginCommand;
     Game_Interpreter.prototype.pluginCommand = function(command, args) {
         _Game_Interpreter_pluginCommand.call(this, command, args);
         if (command === 'PartyGuests') {
             switch (args[0]) {
-				case 'add':
-					$gameSystem.addToPartyGuests(Number(args[1]));
-					break;
-				case 'remove':
-					$gameSystem.removeFromPartyGuests(Number(args[1]));
-					break;
+                case 'add':
+                    $gameParty.addToPartyGuests(Number(args[1]));
+                    break;
+                case 'remove':
+                    $gameParty.removeFromPartyGuests(Number(args[1]));
+                    break;
             }
         }
     };
-	
-	// ========= Game_System extra functions =========
-	Game_System.prototype.addToPartyGuests = function(actorId) {
+    
+    // ========= End of Plugin interpreter commands setup =========
+    
+    var _Game_Party_initialize = Game_Party.prototype.initialize;
+    
+    Game_Party.prototype.initialize = function() {
+        _Game_Party_initialize.call(this, arguments);
+        this._cgn_initGuests();
+    };
+    
+    Game_Party.prototype._cgn_initGuests = function() {
+        this._cgn_partyGuests = [];
+    };
+    
+    Game_Party.prototype._cgn_addGuest = function(actorId) {
+        // if this actor is in the active party, and is not alone, remove from party first
+        if (this._actors.contains(actorId)) {
+            if (this.size() > 1){
+                this.removeActor(actorId);
+            }else{
+                // TODO: alert the user or something
+                return;
+            }
+        }
+        
+        if (!this._cgn_partyGuests.contains(actorId)){
+            this._cgn_partyGuests.push(actorId);
+            $gamePlayer.refresh();
+            $gameMap.requestRefresh();
+        }
+    };
+    
+    Game_Party.prototype._cgn_removeGuest = function(actorId){
+        if (this._cgn_partyGuests.contains(actorId)){
+            this._cgn_partyGuests.splice(this._cgn_partyGuests.indexOf(actorId), 1);
+            $gamePlayer.refresh();
+            $gameMap.requestRefresh();
+        }
+    };
+    
+    // ============ publicly visible functions =============
+    Game_Party.prototype.addToPartyGuests = function(actorId) {
         $gameParty._cgn_addGuest(actorId);
     };
 
-    Game_System.prototype.removeFromPartyGuests = function(actorId) {
+    Game_Party.prototype.removeFromPartyGuests = function(actorId) {
         $gameParty._cgn_removeGuest(actorId);
     };
-	
-	// ========= End of Plugin interpreter commands setup =========
-	
-	var _Game_Party_initialize = Game_Party.prototype.initialize;
-	
-	Game_Party.prototype.initialize = function() {
-		_Game_Party_initialize.call(this, arguments);
-		this._cgn_initGuests();
-	};
-	
-	Game_Party.prototype._cgn_initGuests = function() {
-		this._cgn_partyGuests = [];
-	};
-	
-	Game_Party.prototype._cgn_addGuest = function(actorId) {
-		// if this actor is in the active party, and is not alone, remove from party first
-		if (this._actors.contains(actorId)) {
-			if (this.size() > 1){
-				this.removeActor(actorId);
-			}else{
-				// TODO: alert the user or something
-				return;
-			}
-		}
-		
-		if (!this._cgn_partyGuests.contains(actorId)){
-			this._cgn_partyGuests.push(actorId);
-			$gamePlayer.refresh();
-			$gameMap.requestRefresh();
-		}
-	};
-	
-	Game_Party.prototype._cgn_removeGuest = function(actorId){
-		if (this._cgn_partyGuests.contains(actorId)){
-			this._cgn_partyGuests.splice(this._cgn_partyGuests.indexOf(actorId), 1);
-			$gamePlayer.refresh();
-			$gameMap.requestRefresh();
-		}
-	};
+    
+    Game_Party.prototype.hasPartyGuest = function(actorId) {
+        return this._cgn_partyGuests.contains(actorId);
+    };
 })();
